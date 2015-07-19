a3329f8cd0a4665f093228674c15ab42dce9c66a
cidneyhamilton <chamilton@alum.wellesley.edu>
2015-07-19 11:47:48 -0400

    SPWF 2016 base theme.

---
+++ _partials/404-content.php
@@ -0,0 +1,24 @@
+<article id="post-0" class="post error404 not-found hentry">
+  <header class="entry-header">
+    <h1 class="entry-title">Page not found!</h1>  
+  </header>
+
+  <div class="entry-content">
+	<p>
+	Oh no! Something appears to be wrong.
+	</p>
+
+    <p>
+    If you're looking for a specific attraction, it might not have been confirmed for this year yet.
+    Perhaps try <a href="http://2015.steampunkworldsfair.com<?php echo $_SERVER['REQUEST_URI']; ?>" target_"blank">looking for something from last year's SPWF?</a>
+    </p>
+
+    <p>
+    Or <a href="http://2016.steampunkworldsfair.com/contact">let us know</a> if we have a broken link on our website.
+    </p>
+    
+    <p>Or search for it here.</p>
+
+    <?php get_template_part('searchform'); ?>
+  </div><!-- .entry-content -->
+</article><!-- #post-0 -->
---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'spwf_styles' );
+
+function spwf_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+
+add_action( 'after_setup_theme', 'spwf_setup' );
+
+function spwf_setup() {
+
+  $defaults = array(
+      'default-image' => get_stylesheet_directory_uri() . '/images/header.png'
+  );
+
+  add_theme_support( 'custom-header', $defaults);
+}
+
+?>
